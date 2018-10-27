2d6c667ac1f429057eb1998158ec3fc1f59b5a50
Olga Karpenko <okarpenko@usfca.edu>
2018-10-27 16:22:45 -0700

    Starter code for lab 4.

---
+++ src/recommender/MovieRecommenderDriver.java
@@ -0,0 +1,10 @@
+package recommender;
+
+/** The driver class for lab 4 */
+public class MovieRecommenderDriver {
+
+    public static void main(String[] args) {
+        // FILL IN CODE and add other classes to this project
+
+    }
+}
